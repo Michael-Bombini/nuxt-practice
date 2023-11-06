9db086870e76333ac4c05780670d7ef62c38b07e
Michael Bombini <bombini.michael@gmail.com>
2023-11-06 09:52:21 +0100

    modified sidebase auth

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,6 +1,7 @@
 import { NuxtAuthHandler } from "#auth";
 import GithubProvider from "next-auth/providers/github";
 export default NuxtAuthHandler({
+  secret: process.env.SIDEBASE_SECRET,
   providers: [
     // @ts-expect-error
     GithubProvider.default({
